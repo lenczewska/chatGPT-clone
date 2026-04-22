2265fecbba6e23df041095bd62d76c2d6193d204
Lenczewska <lenczewska@mail.ru>
2026-04-22 09:24:55 +0400

    remove .env and node_modules from tracking

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,11 +1,15 @@
-import express from "express"
-import { getUser, loginUser, registerUser } from "../controllers/userController.js";
-
+import express from "express";
+import {
+  getUser,
+  loginUser,
+  registerUser,
+} from "../controllers/userController.js";
+import { protect } from "../middlewares/auth.js";
 
 const userRouter = express.Router();
 
-userRouter.post('/register', registerUser)
-userRouter.post('/login',loginUser)
-userRouter.get('/data', protect, getUser)
+userRouter.post("/register", registerUser);
+userRouter.post("/login", loginUser);
+userRouter.get("/data", protect, getUser);
 
 export default userRouter;
